75f37dfffc347c154348f55a740f9a7ecf5b9870
Tsunehisa Iwano <t-iwano@axsh.net>
2015-09-09 19:09:03 +0900

    Added Get api for images.

--- api/image/image.go
+++ api/image/image.go
@@ -1,16 +1,17 @@
 package image
 
 import "fmt"
+import "github.com/t-iwano/wakame_api_client/http"
 
 type Endpoint struct {
 }
 
 func (endpoint *Endpoint) Index() {
-     fmt.Println("Show image list.")
+     http.SendRequest("GET", "images", nil)
 }
 
-func (endpoint *Endpoint) Show() {
-     fmt.Println("Show image.")
+func (endpoint *Endpoint) Show(id string) {
+     http.SendRequest("GET", "images", id)
 }
 
 func (endpoint *Endpoint) Create() {
